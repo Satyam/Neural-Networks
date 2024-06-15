2a8cca6176b4e78238cc453d9a2146df866db726
Satyam <satyam@satyam.com.ar>
2024-06-15 19:21:37 +0200

    In `neutalNetwork.js`: - renamed the internal properties and made them private. - Fixed, in `feedForward` didn't count correctly the depth of the network. - Also, dropped some intermediate variables which were not really useful - Changed `train` but I cannot yet figure it out correctly.

--- Dev Article/neuralNetwork.js
+++ Dev Article/neuralNetwork.js
@@ -1,29 +1,33 @@
 class NeuralNetwork {
+  #sizes;
+  #weights;
+  #biases;
+  #layers;
   constructor(...sizes) {
-    this.sizes =
+    this.#sizes =
       sizes.length === 1 && Array.isArray(sizes[0]) ? sizes[0] : sizes;
-    const steps = this.sizes.length - 1;
-    this.allWeights = Array.from({ length: steps }, (_, i) =>
+    const steps = this.#sizes.length - 1;
+    this.#weights = Array.from({ length: steps }, (_, i) =>
       new Matrix(sizes[i + 1], sizes[i]).randomize(-1, 1)
     );
-    this.allBiases = Array.from({ length: steps }, (_, i) =>
+    this.#biases = Array.from({ length: steps }, (_, i) =>
       new Vector(sizes[i + 1]).randomize(-1, 1)
     );
-    this.layers = Array.from(
-      { length: this.sizes.length },
+    this.#layers = Array.from(
+      { length: this.#sizes.length },
       (_, i) => new Vector(sizes[i])
     );
   }
 
   feedForward(inputs) {
-    this.layers[0] = new Vector(inputs);
-    for (let step = 0; step < this.sizes - 1; step++) {
-      let layer = this.layers[step];
-      const weights = this.allWeights[step];
-      const biases = this.allBiases[step];
-      this.layers[step + 1] = weights.times(layer).add(biases).forEach(sigmoid);
+    this.#layers[0] = new Vector(inputs);
+    for (let step = 0; step < this.#sizes.length - 1; step++) {
+      this.#layers[step + 1] = this.#weights[step]
+        .times(this.#layers[step])
+        .add(this.#biases[step])
+        .forEach((val, i) => sigmoid(val));
     }
-    return this.layers.at(-1);
+    return this.#layers.at(-1);
   }
 
   train(inputs, target, learningRate) {
@@ -32,26 +36,38 @@ class NeuralNetwork {
     const errorsOutput = output.map((o, i) => {
       let errOut = target[i] - o;
       const weightFactor = learningRate * errOut * o * (1 - o);
-      for (let j = 0; j < this.hiddenSize; j++) {
-        this.weightsHiddenToOutput[i][j] += weightFactor * this.hiddenLayer[j];
+      for (let j = 0; j < this.#sizes[1]; j++) {
+        this.#weights[1].set(
+          i,
+          j,
+          this.#weights[1].get(i, j) + weightFactor * this.#layers[1][j]
+        );
+        //   this.weightsHiddenToOutput[i][j] += weightFactor * this.hiddenLayer[j];
       }
-      this.biasOutput[i] += learningRate * errOut;
+      this.#biases[1] = this.#biases[1].times(learningRate * errOut);
+      // this.biasOutput[i] += learningRate * errOut;
       return errOut;
     });
 
-    for (let i = 0; i < this.hiddenSize; i++) {
+    for (let i = 0; i < this.#sizes[1]; i++) {
       let errHidden = 0;
-      for (let j = 0; j < this.outputSize; j++) {
-        errHidden += this.weightsHiddenToOutput[j][i] * errorsOutput[j];
+      for (let j = 0; j < this.#sizes[2]; j++) {
+        errHidden += this.#weights[1].get(j, i) * errorsOutput[j];
+        // errHidden += this.weightsHiddenToOutput[j][i] * errorsOutput[j];
       }
-      this.biasHidden[i] += learningRate * errHidden;
+      this.#biases[1][i] += learningRate * errHidden;
       const factor =
         learningRate *
         errHidden *
-        this.hiddenLayer[i] *
-        (1 - this.hiddenLayer[i]);
-      for (let j = 0; j < this.inputSize; j++) {
-        this.weightsInputToHidden[i][j] += factor * inputs[j];
+        this.#layers[1][i] *
+        (1 - this.#layers[1][i]);
+      for (let j = 0; j < this.#sizes[0]; j++) {
+        this.#weights[0].set(
+          i,
+          j,
+          this.#weights[0].get(i, j) + factor * inputs[j]
+        );
+        // this.weightsInputToHidden[i][j] += factor * inputs[j];
       }
     }
   }
