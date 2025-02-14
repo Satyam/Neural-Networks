c85d2723fcd0cd824a70a4ab1282624d4c2c1c0c
Satyam <satyam@satyam.com.ar>
2025-02-14 18:41:52 +0100

    Changed "trainingIterations" into "repetitions" which then gets multiplied by the data size. Treats the case of no repetition (just once) as a special case with no intermediate array. Added `type="number"` in the `<inputs>` for most of the parameters

--- Dev Article/train.js
+++ Dev Article/train.js
@@ -3,26 +3,33 @@ import { readFloat, readInt, getRandomPoint, outputEncoding } from './utils.js';
 export function train(neuralNetwork) {
   const trainingDataSize = readInt('#trainingDataSize');
   const learningRate = readFloat('#learningRate');
-  const trainingIterations = readInt('#trainingIterations');
+  const repetitions = readInt('#repetitions');
 
   const t1 = performance.now();
 
-  const trainingData = Array.from(
-    {
-      // Adjust the number of data points as needed
-      length: trainingDataSize,
-    },
-    () => {
-      const [x, y] = getRandomPoint();
+  if (repetitions > 1) {
+    const trainingData = Array.from(
+      {
+        // Adjust the number of data points as needed
+        length: trainingDataSize,
+      },
+      () => {
+        const [x, y] = getRandomPoint();
 
-      return { x, y, encoding: outputEncoding(x, y) };
+        return { x, y, encoding: outputEncoding(x, y) };
+      }
+    );
+
+    for (let i = repetitions * trainingDataSize; i--; ) {
+      const { x, y, encoding } =
+        trainingData[Math.floor(Math.random() * trainingDataSize)];
+      neuralNetwork.train([x, y], encoding, learningRate);
     }
-  );
-
-  for (let i = trainingIterations; i--; ) {
-    const { x, y, encoding } =
-      trainingData[Math.floor(Math.random() * trainingDataSize)];
-    neuralNetwork.train([x, y], encoding, learningRate);
+  } else {
+    for (let i = trainingDataSize; i--; ) {
+      const [x, y] = getRandomPoint();
+      neuralNetwork.train([x, y], outputEncoding(x, y), learningRate);
+    }
   }
   console.log('train total', performance.now() - t1);
 }
