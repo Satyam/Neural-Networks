3649728843ae76ae030241df0150cac8f6b1c506
Satyam <satyam@satyam.com.ar>
2025-02-16 19:04:47 +0100

    Made the code much nicier but still the result is no better than random.

--- Mnist/train.mjs
+++ Mnist/train.mjs
@@ -2,55 +2,68 @@ import { createReadStream } from 'node:fs';
 import { createInterface } from 'node:readline';
 import { NeuralNetwork } from '../Dev Article/neuralNetwork.js';
 
-const readTrainLine = createInterface({
-  input: createReadStream('./mnist_train.csv'),
-  crlfDelay: Infinity,
-});
-
 const neuralNetwork = new NeuralNetwork(784, 15, 10);
 
-const output = Array(10).fill(0);
-let prevVal = 0;
-let limit = 100000;
-const tTrain = performance.now();
-readTrainLine.on('line', (line) => {
-  if (limit) {
-    limit--;
-    const [value, ...pixels] = line.split(',').map((v) => parseInt(v, 10));
-    output[prevVal] = 0;
-    output[value] = 1;
-    prevVal = value;
-    neuralNetwork.train(pixels, output, 0.1);
-  } else {
-    readTrainLine.close();
-  }
-});
-readTrainLine.on('close', () => {
-  console.log('train total', performance.now() - tTrain);
+const decodeLine = (line) => line.split(',').map((v) => parseInt(v, 10));
 
-  const readTestLine = createInterface({
-    input: createReadStream('./mnist_test.csv'),
-    crlfDelay: Infinity,
-  });
-  const tTest = performance.now();
-  let numTests = 0;
-  let success = 0;
-  readTestLine.on('line', (line) => {
-    numTests++;
-    const [value, ...pixels] = line.split(',').map((v) => parseInt(v, 10));
-    output[prevVal] = 0;
-    output[value] = 1;
-    prevVal = value;
-    const found = neuralNetwork.feedForward(pixels);
-    if (value === found.indexOf(Math.max(...found))) success++;
-  });
+const encodeValue = (value) =>
+  Array.from({ length: 10 }, (_, i) => (value === i ? 1 : 0));
 
-  readTestLine.on('close', () => {
-    console.log(
-      'test total',
-      performance.now() - tTest,
-      'success',
-      success / numTests
-    );
+const decodeValue = (output) => output.indexOf(Math.max(...output));
+
+await train(neuralNetwork, './mnist_train.csv', 3);
+await test(neuralNetwork, './mnist_test.csv');
+
+function train(
+  neuralNetwork,
+  fileName,
+  learningRate,
+  limit = Number.MAX_SAFE_INTEGER
+) {
+  return new Promise((resolve) => {
+    const readTrainLine = createInterface({
+      input: createReadStream(fileName),
+      crlfDelay: Infinity,
+    });
+    const tTrain = performance.now();
+    readTrainLine.on('line', (line) => {
+      if (limit) {
+        limit--;
+        const [value, ...pixels] = decodeLine(line);
+        neuralNetwork.train(pixels, encodeValue(value), learningRate);
+      } else {
+        readTrainLine.close();
+      }
+    });
+    readTrainLine.on('close', () => {
+      console.log('Train Total Time', (performance.now() - tTrain).toFixed(0));
+      resolve();
+    });
   });
-});
+}
+
+function test(neuralNetwork, filename) {
+  return new Promise((resolve) => {
+    const readTestLine = createInterface({
+      input: createReadStream(filename),
+      crlfDelay: Infinity,
+    });
+    let numTests = 0;
+    let success = 0;
+    const tTest = performance.now();
+    readTestLine.on('line', (line) => {
+      numTests++;
+      const [value, ...pixels] = decodeLine(line);
+      const found = neuralNetwork.feedForward(pixels);
+      if (value === decodeValue(found)) success++;
+    });
+
+    readTestLine.on('close', () => {
+      console.log(
+        `Test total time: ${(performance.now() - tTest).toFixed(0)}
+Success: ${((success / numTests) * 100).toFixed(2)}%`
+      );
+      resolve(success);
+    });
+  });
+}
